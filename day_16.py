b47b079919f0e226097c93fc950c3bd1b51f846f
Catalin Cristu <catalin.cristu@gmail.com>
2017-12-17 23:09:50 +0200

    Pre-decode dance moves to speed up execution for solution to day 16

--- day_16.py
+++ day_16.py
@@ -23,25 +23,38 @@ class ProgramGroup(object):
         self.programs = list(string.ascii_lowercase[0:size])
 
     def dance(self, moves, repeat=1):
+        moves = [self._decode(m) for m in moves]
+        size = len(self.programs)
         for _ in range(repeat):
             for move in moves:
                 if move[0] == 's':
-                    n = int(move[1:])
-                    size = len(self.programs)
-                    self.programs = self.programs[-n:] + \
-                        self.programs[0:size - n]
+                    i = move[1]
+                    self.programs = self.programs[-i:] + \
+                        self.programs[0:size - i]
                 elif move[0] == 'x':
-                    k = move.index('/')
-                    i = int(move[1:k])
-                    j = int(move[k + 1:])
+                    i = move[1]
+                    j = move[2]
                     self.programs[i], self.programs[j] = self.programs[j], self.programs[i]
                 elif move[0] == 'p':
-                    k = move.index('/')
-                    a = move[1:k]
-                    b = move[k + 1:]
+                    a = move[1]
+                    b = move[2]
                     i = self.programs.index(a)
                     j = self.programs.index(b)
                     self.programs[i], self.programs[j] = self.programs[j], self.programs[i]
+
+    def _decode(self, move):
+        if move[0] == 's':
+            return ('s', int(move[1:]))
+        elif move[0] == 'x':
+            k = move.index('/')
+            i = int(move[1:k])
+            j = int(move[k + 1:])
+            return ('x', i, j)
+        elif move[0] == 'p':
+            k = move.index('/')
+            a = move[1:k]
+            b = move[k + 1:]
+            return ('p', a, b)
 
     def __str__(self):
         return ''.join(self.programs)
