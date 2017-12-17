cb8b6bd6b8aabe5fd60a8aa633a724d15af44c81
Catalin Cristu <catalin.cristu@gmail.com>
2017-12-17 10:38:03 +0200

    Inline dance moves and add a timing utility

--- day_16.py
+++ day_16.py
@@ -3,6 +3,18 @@
 import unittest
 import sys
 import string
+import time
+from datetime import timedelta
+
+
+def timing(fn):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = fn(*args, **kwargs)
+        end = time.time()
+        print("%s took %s" % (fn.__name__, timedelta(seconds=end - start)))
+        return result
+    return wrapper
 
 
 class ProgramGroup(object):
@@ -10,32 +22,26 @@ class ProgramGroup(object):
     def __init__(self, size):
         self.programs = list(string.ascii_lowercase[0:size])
 
-    def dance(self, move):
-        if move[0] == 's':
-            n = int(move[1:])
-            self.spin(n)
-        elif move[0] == 'x':
-            k = move.index('/')
-            i = int(move[1:k])
-            j = int(move[k + 1:])
-            self.exchange(i, j)
-        elif move[0] == 'p':
-            k = move.index('/')
-            a = move[1:k]
-            b = move[k + 1:]
-            self.partner(a, b)
-
-    def spin(self, n):
-        size = len(self.programs)
-        self.programs = self.programs[-n:] + self.programs[0:size - n]
-
-    def exchange(self, i, j):
-        self.programs[i], self.programs[j] = self.programs[j], self.programs[i]
-
-    def partner(self, a, b):
-        i = self.programs.index(a)
-        j = self.programs.index(b)
-        self.exchange(i, j)
+    def dance(self, moves, repeat=1):
+        for _ in range(repeat):
+            for move in moves:
+                if move[0] == 's':
+                    n = int(move[1:])
+                    size = len(self.programs)
+                    self.programs = self.programs[-n:] + \
+                        self.programs[0:size - n]
+                elif move[0] == 'x':
+                    k = move.index('/')
+                    i = int(move[1:k])
+                    j = int(move[k + 1:])
+                    self.programs[i], self.programs[j] = self.programs[j], self.programs[i]
+                elif move[0] == 'p':
+                    k = move.index('/')
+                    a = move[1:k]
+                    b = move[k + 1:]
+                    i = self.programs.index(a)
+                    j = self.programs.index(b)
+                    self.programs[i], self.programs[j] = self.programs[j], self.programs[i]
 
     def __str__(self):
         return ''.join(self.programs)
@@ -45,27 +51,24 @@ class Test(unittest.TestCase):
 
     def test_dance(self):
         pg = ProgramGroup(5)
-        moves = [('s1', 'eabcd'), ('x3/4', 'eabdc'), ('pe/b', 'baedc')]
-        for m, e in moves:
-            pg.dance(m)
-            self.assertEqual(e, str(pg))
+        moves = ['s1', 'x3/4', 'pe/b']
+        pg.dance(moves)
+        self.assertEqual('baedc', str(pg))
 
 
 def main():
     if len(sys.argv) >= 2:
         for fname in sys.argv[1:]:
             with open(fname, 'r') as infile:
-                pg = ProgramGroup(16)
                 moves = []
                 for line in infile:
                     moves.extend(line.split(','))
-                for move in moves:
-                    pg.dance(move)
-                print("'%s' -> %s" % (fname, pg))
-                for _ in range(1000000000):
-                    for move in moves:
-                        pg.dance(move)                    
-                print("'%s' -> %s" % (fname, pg))
+                pg = ProgramGroup(16)
+                pg.dance(moves)
+                print("'%s' after first dance -> %s" % (fname, pg))
+                k = 1000000000
+                pg.dance(moves, repeat=k)
+                print("'%s' after %d dances -> %s" % (fname, k, pg))
     else:
         unittest.main()
 
