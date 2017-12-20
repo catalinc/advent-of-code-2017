5311d5b5fe477bc93b898afb30a44674feb112a4
Catalin Cristu <catalin.cristu@gmail.com>
2017-12-20 09:46:53 +0200

    Incomplete solution to day 18 sub-problem 2

--- day_18.py
+++ day_18.py
@@ -8,33 +8,34 @@ Instruction = collections.namedtuple('Instruction', 'code, op1, op2')
 
 
 class Program(object):
+    RUNNING = 0
+    WAITING_FOR_IO = 1
+    TERMINATED = 2
 
-    def __init__(self):
+    def __init__(self, pid, filename):
+        self.pc = 0
         self.instructions = []
-
-    def load(self, fname):
-        with open(fname, 'r') as infile:
+        self.pid = pid
+        self.registers = collections.defaultdict(int)
+        self.registers['p'] = pid
+        self.inbox = []
+        self.outbox = []
+        self.state = Program.RUNNING
+        with open(filename, 'r') as infile:
             for line in infile:
+                line = line.rstrip()
                 tokens = line.split(' ')
                 code = tokens[0]
                 op1 = tokens[1]
                 op2 = 0
                 if len(tokens) == 3:
                     op2 = tokens[2]
-                self.instructions.append(Instruction(
-                    code=code, op1=op1, op2=op2))
+                self.instructions.append(Instruction(code=code, op1=op1, op2=op2))
 
-
-class SoundVm(object):
-
-    def __init__(self):
-        self.registers = collections.defaultdict(int)
-        self.pc = 0
-        self.last_sound = -1
-
-    def run(self, program):
-        self.reset()
-        for instr in program.instructions:
+    def run(self):
+        if 0 <= self.pc < len(self.instructions):
+            self.state = Program.RUNNING
+            instr = self.instructions[self.pc]
             jump = 1
             if instr.code == 'set':
                 self.registers[instr.op1] = self._value(instr.op2)
@@ -49,23 +50,34 @@ class SoundVm(object):
                 if cond > 0:
                     jump = self._value(instr.op2)
             elif instr.code == 'snd':
-                freq = self._value(instr.op1)
-                if freq > 0:
-                    self.last_sound = freq
+                v = self._value(instr.op1)
+                self.outbox.append(v)
+            elif instr.code == 'rcv':
+                if self.inbox:
+                    v = self.inbox.pop(0)
+                    r = instr.op1
+                    self.registers[r] = v
+                else:
+                    jump = 0
+                    self.state = Program.WAITING_FOR_IO
             self.pc += jump
-            if self.pc < 0 or self.pc >= len(program.instructions):
-                return self.last_sound
+        else:
+            self.state = Program.TERMINATED
 
     def _value(self, s):
         if 'a' <= s <= 'z':
             return self.registers[s]
         return int(s)
 
-    def reset(self):
-        self.registers.clear()
-        self.pc = 0
-        self.last_sound = -1
 
+class System(object):
+
+    def __init__(self, filename):
+        self.program_0 = Program(0, filename)
+        self.program_1 = Program(1, filename)
+
+    def run(self):
+        pass
 
 class Test(unittest.TestCase):
 
@@ -76,14 +88,22 @@ class Test(unittest.TestCase):
         first = Instruction(code='set', op1='a', op2='1')
         self.assertEqual(first, p.instructions[0])
 
+    def test_run_program(self):
+        p = Program()
+        p.load('day_18.test')
+        vm = Program()
+        self.assertEqual(4, vm.run(p))
+
 
 def main():
     if len(sys.argv) >= 2:
-        vm = SoundVm()
+        vm = Program()
         for fname in sys.argv[1:]:
             p = Program()
             p.load(fname)
             print("%s -> %d" % (fname, vm.run(p)))
+    else:
+        unittest.main()
 
 
 if __name__ == '__main__':
