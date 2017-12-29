d5a6d478dddff7ebaf16b5db10206ffb486ded1d
Catalin Cristu <catalin.cristu@gmail.com>
2017-12-29 19:26:59 +0200

    Add solution to day 17 sub-problem 2

--- day_17.py
+++ day_17.py
@@ -4,7 +4,7 @@ import unittest
 import sys
 
 
-def spinlock(steps, times):
+def spin_lock(steps, times):
     data = [0]
     i, j = 1, 0
     while i <= times:
@@ -15,15 +15,31 @@ def spinlock(steps, times):
     return data[j]
 
 
+def spin_lock_2(steps, times):
+    i, j, size = 1, 0, 1
+    result = 0
+    while i <= times:
+        j = (j + 1 + steps) % size
+        if j == 0:
+            result = i
+        size += 1
+        i += 1
+    return result
+
+
 class Test(unittest.TestCase):
 
-    def test_spinlock(self):
-        self.assertEqual(638, spinlock(3, 2017))
+    def test_spin_lock(self):
+        self.assertEqual(638, spin_lock(3, 2017))
+
+    def test_spin_lock_2(self):
+        self.assertEqual(9, spin_lock_2(3, 9))
 
 
 def main():
     if len(sys.argv) == 2 and sys.argv[1] == '-run':
-        print(spinlock(337, 50000000))
+        print(spin_lock(337, 2017))
+        print(spin_lock_2(337, 50_000_000))
     else:
         unittest.main()
 
